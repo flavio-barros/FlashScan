759a98beb408650426d88bcb4cfcb0f05b521f14
amanda-s <amanda_0.s@hotmail.com>
2016-11-03 09:11:22 -0300

    criação da activity de detalhes do documento.

---
+++ app/src/main/java/br/ufc/quixada/android/flashscan/ActivityDetalhesDocumento.java
@@ -0,0 +1,15 @@
+package br.ufc.quixada.android.flashscan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityDetalhesDocumento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_documento);
+
+
+    }
+}
